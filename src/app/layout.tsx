74d2f33770ad3e77afb051a741245c232027a440
sanjeevnc-claw <sanjeev.nc20@gmail.com>
2026-02-12 18:53:51 +0530

    Add src/app/layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,4 @@
+export const metadata = { title: 'site-mljhp5r0' };
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return <html lang="en"><body>{children}</body></html>;
+}
